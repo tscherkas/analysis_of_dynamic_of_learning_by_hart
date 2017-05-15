e09c4ba900e74882921dde865738987b399777a8
tscherkas <tscherkas.it@gmail.com>
2017-05-15 16:55:58 +0300

    temp

---
+++ GUI_module/GUI_module/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Commands;
+using System.Windows.Input;
+
+namespace GUI_module
+{
+    public class MainWindowViewModel
+    {
+
+        public string Password { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.Login = new DelegateCommand<object>(this.OnLogin);
+        }
+
+        private void OnLogin(object obj)
+        {
+            if(Password!="210901")
+                throw new NotImplementedException();
+        }
+
+        public ICommand Login { get; set; }
+        public ICommand StartQuestionarie { get; set; }
+        public ICommand ShowTheory { get; set; }
+    }
+}
